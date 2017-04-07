3244082e6f98ec5552bceeb8b893068c6a28d7be
Andrey Seleznov <andrewaka39@gmail.com>
2017-04-07 17:11:39 +0100

    add origins to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', 'https://stark-gorge-55434.herokuapp.com/'
 
     resource '*',
       headers: :any,
